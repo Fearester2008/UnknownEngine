88a4902815c5c52414b7582930a4f35cd3926691
Fearester2008 <131034025+Fearester2008@users.noreply.github.com>
2023-04-22 11:22:43 +0300

    Update ModifiersState.hx

--- source/ModifiersState.hx
+++ source/ModifiersState.hx
@@ -31,7 +31,7 @@ using StringTools;
 
 class ModifiersState extends MusicBeatState
 {
-	var checker:FlxBackdrop = new FlxBackdrop(Paths.image('Modi_Checker'), 0.2, 0.2, true, true);
+	var checker:FlxBackdrop = new FlxBackdrop(Paths.image('Modi_Checker'), XY, 20,20);
 	var gradientBar:FlxSprite = new FlxSprite(0,0).makeGraphic(FlxG.width, 300, 0xFFAA00AA);
 	var side:FlxSprite = new FlxSprite(0).loadGraphic(Paths.image('Modi_Bottom'));
 	
